50ba10405c39f71ed8e9d8b57584fdfc08578055
sboon <sboon@265bcc82-57df-11dd-8522-2d0c19f12d9e>
2024-12-24 10:54:53 +0000

    Updates to use the new 10 sec stream and csv
    git-svn-id: https://quarchserver4.ds.quarch.com/svn/Software/Application%20Notes/AN-030%20-%20Merging%20in%20custom%20data@10741 265bcc82-57df-11dd-8522-2d0c19f12d9e

--- QpsDataMergeExample.py
+++ QpsDataMergeExample.py
@@ -5,18 +5,14 @@ This example is based on our blog: analysing the energy and water consumption of
 This example pulls in custom user data from another source (a water meter in this case) and creates new channels in QPS
 
 It them imports the data into those channels.  Note that the files supplied here will import volume/flow data into
-any QPS trace that is at least 29 minutes long.  You can edit the water_rates.csv file or provide your own easily.
-
-To get this data set for water, we captured using a multi-channel PAM, using a digital channel to capture the pulses
-from the water meter.  We then used the 'WaterProcessor.py' script, hacked together quickly to process the CSV export
-from QPS and turn it into a small number of user data points that we can upload. Custom User Data is intended to
-be 'sparse' (ie: perhaps 1 sample per second), adding a lot more can be memory intensive and take some time.
+any QPS trace that is at least 10 seconds long.  You can use this as a tool, pick your own qps streams and edit the csv
+file or provide your own.
 
 ########### VERSION HISTORY ###########
 
 23/04/2024 - Andy Norrie    - First Version
 10/12/2024 - Graham Seed    - Use of "$stream import" command to speedup loading CSV data
-
+23/12/2024  - Stuart Boon   - Making the script more generic and providing a qps stream and csv file.
 ########### REQUIREMENTS ##############
 
 1- Python (3.x recommended)
@@ -32,19 +28,27 @@ be 'sparse' (ie: perhaps 1 sample per second), adding a lot more can be memory i
 
 ########### INSTRUCTIONS ##############
 
-1- Capture a trace on QPS then 'Save AS'
-2- Run the script and follow the instructions on screen.
-3- Select your trace when prompted (it must be long enough to take all the user data, 29 minutes for this example)
+1- Run the script and follow the instructions on screen.
+2- Select your trace when prompted and choose if you want to make a copy to add the data to or add to the original file
+3- Select the csv data to add
+4- Look at the QPS main chart with the newly added data.
+    Try hiding all channels except the newly added ones to see them clearly.
+5- End the script and look through the code and comment for a better understandin of how it works.
 
 #######################################
 """
 
-# Import other libraries used in the examples
+# Import Libs
+import time
+import datetime
+import shutil
+import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 # Import QPS functions
-from quarchpy import qpsInterface, requiredQuarchpyVersion
+from quarchpy.qps import closeQps
+from quarchpy import qpsInterface, requiredQuarchpyVersion,isQpsRunning, startLocalQps
 
 
 def main():
@@ -56,19 +60,23 @@ def main():
     print("---------------------------------------\n\n")
 
     # This is the file containing the user data that we will merge into the trace
-    # The .csv file must contain a header of the form:
-    # "Time mS","Rate Flow mL/s","Total Volume L", ...
-    # That is, a < time, unit > column followed by < channel, group, unit > columns
-    merge_filename = "\\EDIT-PATH-FILE\\water_rates.csv"  # eg; "c:\\temp\\csv_files\\water_rates.csv"
     # Version 2.1.24 or higher expected for this application note
-    requiredQuarchpyVersion("2.1.24")
+    requiredQuarchpyVersion("2.3.0")
+
+    #Start QPS if it is not already running, Do this first to allow plenty start up time.
+    if not isQpsRunning():
+        startLocalQps()
+        time.sleep(1)
 
     # Ask the user to select the QPS recording to open. This is the one that we will merge the data into
     root = Tk()
     root.withdraw()
-    filetypes = (("QPS files", "*.qps"),)
     print("Please open the recording.")
-    file_path = askopenfilename(title="Open the recording",filetypes=filetypes)
+    file_path = askopenfilename(title="Open the recording",filetypes=(("QPS files", "*.qps"),))
+    if input('This recording file will be overwritten by default.'
+             '\n Would you like to make a copy? (y/n): ').lower().strip() == 'y':
+        file_path=copy_recording_folder_add_timestamp(file_path)
+    merge_filename = askopenfilename(title="Open csv file",filetypes=(("csv files","*.csv"),))
 
     # Connect to local QPS
     myQPS = qpsInterface()
@@ -96,6 +104,31 @@ def main():
     cmd_result = myQPS.sendCmdVerbose(command)
     print("Importing of CSV values: " + cmd_result)
 
+    input("Take a minute to look at the QPS window and the new channels that have been added"
+          "\nPress Enter to close QPS and exit the script.")
+    input("Are you sure?")
+    closeQps() # Close QPS at the end of the script.
+
+    return # END MAIN
+
+
+def copy_recording_folder_add_timestamp(file_path):
+    ''' A utility function to copy the recoding folder and point to the new recording file '''
+    # Get the folder containing the selected file
+    folder_path = os.path.dirname(file_path)
+
+    # Generate a new folder name with timestamp
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    new_folder_name = f"{os.path.basename(folder_path)}_{timestamp}"
+    parent_dir = os.path.dirname(folder_path) #whats this todo
+    destination_folder = os.path.join(parent_dir, new_folder_name)
+
+    # Copy the folder
+    shutil.copytree(folder_path, destination_folder)
+
+    # Compute the path to the copied file
+    new_file_path = os.path.join(destination_folder, os.path.basename(file_path))
+    return new_file_path
 
 # Calling the main() function
 if __name__ == "__main__":
